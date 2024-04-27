57c0425358c35aa388480f3007e9d9f7bb017b10
Raymond <86973702+RaymondCao001@users.noreply.github.com>
2024-04-27 13:58:25 +1000

    Code

---
+++ HotelBookingSystem/HotelBookingSystem/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HotelBookingSystem
+//
+//  Created by Relex on 27/4/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HotelBookingSystem/HotelBookingSystem/HotelBookingSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotelBookingSystemApp.swift
+//  HotelBookingSystem
+//
+//  Created by Relex on 27/4/2024.
+//
+
+import SwiftUI
+
+@main
+struct HotelBookingSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
