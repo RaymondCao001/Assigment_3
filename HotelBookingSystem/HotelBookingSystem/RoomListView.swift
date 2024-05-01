5b0ea0a38aa1bd148a3159783acf9466d345a365
GalaZhou <gaalaa.zhou@gmail.com>
2024-05-01 14:27:12 +1000

    v1.3

--- HotelBookingSystem/HotelBookingSystem/RoomListView.swift
+++ HotelBookingSystem/HotelBookingSystem/RoomListView.swift
@@ -56,9 +56,10 @@ struct RoomView: View {
 struct RoomListView_Previews: PreviewProvider {
     static var previews: some View {
         RoomListView(rooms: [
-            Room(id: "1", type: "Standard Room", occupancy: 2, bedType: "Queen Size", price: 120.00, area: 45),
-            Room(id: "2", type: "Luxury Room", occupancy: 2, bedType: "King Size", price: 200.00, area: 45),
-            Room(id: "3", type: "Suite", occupancy: 2, bedType: "King Size", price: 400.00, area: 90)
+            Room(id: "1", type: "Standard Room", occupancy: 2, bedType: "Queen Size", price: 120.00, area: 35),
+            Room(id: "2", type: "Superior Room", occupancy: 2, bedType: "Queen Size", price: 160.00, area: 45),
+            Room(id: "3", type: "Luxury Room", occupancy: 2, bedType: "King Size", price: 200.00, area: 45),
+            Room(id: "4", type: "Suite", occupancy: 2, bedType: "King Size", price: 400.00, area: 90)
         ])
     }
 }
